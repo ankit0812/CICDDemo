babb8508cb2cd8a8b076de6ddabf33e3a93c7ba5
ankit0812 <ankitkumargupta0812@gmail.com>
2022-03-09 16:47:21 +0530

    Some code and pod addition

--- CICDDemo/ViewController.swift
+++ CICDDemo/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import DKImagePickerController
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var btnImagePicker: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnImagePickerCalled(_ sender: Any) {
+        let vc = DKImagePickerController()
+        vc.maxSelectableCount = 20
+        vc.assetType = .allPhotos
+        vc.allowMultipleTypes = false
+        vc.showsCancelButton = true
+        vc.didSelectAssets = { [weak self](assets: [DKAsset]) in
+            // self?.manipulateAndUpdateAttachmentList(assets: assets)
+//            MediaEditorVC.manipulateAndUpdateImageList(assets: assets, completion: { imageDataSource in
+//                LCUtils.hideLoaderFrom(view: self?.view ?? UIView())
+//                self?.openImageConfirmationVc(images: imageDataSource)
+//            })
+            print(self)
+            print(assets)
+        }
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
 }
 
